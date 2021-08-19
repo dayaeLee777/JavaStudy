8a5b5c76a95846f625e7b340a35dd7c18a29907d
Daye <ldy1853@naver.com>
2021-08-20 00:43:55 +0900

    백트래킹 BOJ_17136_색종이붙이기

--- src/com/ssafy/IM/BOJ_17136_색종이붙이기.java
+++ src/com/ssafy/IM/BOJ_17136_색종이붙이기.java
@@ -5,25 +5,23 @@ import java.io.BufferedWriter;
 import java.io.IOException;
 import java.io.InputStreamReader;
 import java.io.OutputStreamWriter;
-import java.util.ArrayList;
-import java.util.Arrays;
 import java.util.StringTokenizer;
 
 // https://www.acmicpc.net/problem/17136
+
 public class BOJ_17136_색종이붙이기 {
 
 	static String s;
-	static StringBuilder sb = new StringBuilder();
-	static int N, paper[][];
 	static StringTokenizer st;
-	static boolean isfilled;
-	static ArrayList<Point> pArr;
+	static StringBuilder sb = new StringBuilder();
+
+	static int N, paper[][], res;
+	static int[] papercnt = { 5, 5, 5, 5, 5 };
 
 	public static void main(String[] args) throws IOException {
 
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 		BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
-		pArr = new ArrayList<>();
 		paper = new int[10][10];
 		for (int n = 0; n < 10; n++) {
 			s = br.readLine();
@@ -31,21 +29,15 @@ public class BOJ_17136_색종이붙이기 {
 			for (int m = 0; m < 10; m++) {
 				paper[n][m] = Integer.parseInt(st.nextToken());
 				if (paper[n][m] == 1) {
-					pArr.add(new Point(n, m));
 				}
 			}
 
 		}
-
-		System.out.println(pArr.toString());
-
-		find(0, 0);
-
-		System.out.println(pArr.toString());
-		pArr.sort(null);
-
-		System.out.println(pArr.toString());
-
+		res = Integer.MAX_VALUE;
+		dfs(0,0,0);
+		if(res==Integer.MAX_VALUE)
+			res=-1;
+		sb.append(res);
 		bw.write(sb.toString());
 
 		bw.flush();
@@ -53,81 +45,63 @@ public class BOJ_17136_색종이붙이기 {
 		bw.close();
 	}
 
-	static boolean check(Point p, int size) {
-		if (p.r + size >= 10 || p.c + size >= 10)
-			return false;
-		for (int i = p.r; i <= p.r + size; i++) {
-			if (p.c + size < 10 && paper[i][p.c + size] != 1)
-				return false;
-		}
-		for (int j = p.c; j <= p.c + size; j++) {
-			if (p.r + size < 10 && paper[p.r + size][j] != 1)
-				return false;
+	static boolean check(int r, int c, int size) {
+		for (int i = r; i < r + size; i++) {
+			for (int j = c; j < c + size; j++) {
+				if (i < 0 || i >= 10 || j <0 || j>=10 || paper[i][j] != 1) {
+					return false;
+				}
+			}
 		}
 		return true;
 	}
 
-	static void find(int i, int size) {
-		if (i >= pArr.size())
+	// 색종이를 떼고 붙여가며 붙인 갯수를 구하기
+	static void dfs(int r, int c, int sum) {
+		if (r >= 9 && c > 9) {
+			res = Math.min(res, sum);
+			return;}
+		if (res <= sum) {
 			return;
-		Point p = pArr.get(i);
-		if (size == 5 || !check(p, size)) {
-			pArr.get(i).size = size;
+		}
+		if (c > 9) {
+			dfs(r + 1, 0, sum);
 			return;
-		} else {
-			for (int a = i; a < pArr.size(); a++) {
-				find(a, size + 1);
+		}
+
+		if (paper[r][c] == 1) {
+			for (int i = 4; i >= 0; i--) {
+				if (papercnt[i] > 0 && check(r, c, i+1)) {
+					attack(r, c, i+1);
+					papercnt[i]--;
+					dfs(r, c+1, sum+1);
+					cut(r,c,i+1);
+					papercnt[i]++;
+				}
+
+			}
+		}
+		else
+			dfs(r, c+1, sum);
+
+	}
+
+	// 색종이 붙이기
+	static void attack(int r, int c, int size) {
+		for (int i = r; i < r + size; i++) {
+			for (int j = c; j < c + size; j++) {
+				paper[i][j] = 0;
 			}
 		}
 	}
 
-	static class Point implements Comparable<Point> {
-		int r, c;
-		int size;
-
-		public Point(int r, int c) {
-			super();
-			this.r = r;
-			this.c = c;
-			this.size = 0;
-		}
-
-		@Override
-		public String toString() {
-			return "Point [r=" + r + ", c=" + c + ", size=" + size + "]";
-		}
-
-		@Override
-		public int compareTo(Point o) {
-			int value = (this.size - o.size) * -1;
-			if (value == 0)
-				return this.r - o.r;
-			return value;
-		}
-
-		@Override
-		public int hashCode() {
-			final int prime = 31;
-			int result = 1;
-			result = prime * result + c;
-			result = prime * result + r;
-			return result;
-		}
-
-		@Override
-		public boolean equals(Object obj) {
-			if (this == obj)
-				return true;
-			if (obj == null)
-				return false;
-			if (getClass() != obj.getClass())
-				return false;
-			Point other = (Point) obj;
-			if (c != other.c)
-				return false;
-			if (r != other.r)
-				return false;
-			return true;
+	// 색종이 떼기
+	static void cut(int r, int c, int size) {
+		for (int i = r; i < r + size; i++) {
+			for (int j = c; j < c + size; j++) {
+				paper[i][j] = 1;
+			}
 		}
 	}
+
 }
