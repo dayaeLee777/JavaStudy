f1fe15348bcf2a02f7042b3c216f75cf49b32f6c
Daye <ldy1853@naver.com>
2021-09-07 23:02:41 +0900

    BOJ_4673_셀프넘버

---
+++ src/stage/fuction/BOJ_4673_셀프넘버.java
@@ -0,0 +1,28 @@
+package stage.fuction;
+
+public class BOJ_4673_셀프넘버 {
+	static boolean[] notSelfNum = new boolean[10001];
+	
+	static void d(int n) {
+		int res = n;
+		while(n>0) {
+			res += n%10;
+			n /= 10;
+		}
+		
+		if(res>10000)
+			return;
+		
+		notSelfNum[res] = true;
+		d(res);
+	}
+	
+	public static void main(String[] args) {
+		
+		for(int i=1; i<10001;i++)
+			d(i);
+		for(int i=1; i<10001;i++)
+			if(!notSelfNum[i])	System.out.println(i);
+	}
+
+}
