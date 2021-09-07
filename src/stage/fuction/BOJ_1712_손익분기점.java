b0fa2aa553933a31529b8f1822d0c98b6abdedd7
Daye <ldy1853@naver.com>
2021-09-07 23:58:40 +0900

    BOJ_1712_손익분기점

---
+++ src/stage/fuction/BOJ_1712_손익분기점.java
@@ -0,0 +1,29 @@
+package stage.fuction;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+public class BOJ_1712_손익분기점 {
+
+	static StringTokenizer st;
+
+	public static void main(String[] args) throws IOException {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+		st = new StringTokenizer(br.readLine());
+
+		int A = Integer.parseInt(st.nextToken());
+		int B = Integer.parseInt(st.nextToken());
+		int C = Integer.parseInt(st.nextToken());
+		
+		int res = -1;
+		if (C - B > 0)
+			res = (A / (C - B)) > 0 ? (A / (C - B)) + 1 : -1;
+
+		System.out.println(res);
+
+		br.close();
+	}
+}
